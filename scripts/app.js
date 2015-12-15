7b04dbd3dc16c08bc4925b74ab4731d8d541be73
craigcwaveformlabs <craigmclarke@gmail.com>
2015-12-15 11:41:41 +0000

    investigating scope with parent and sibling controllers

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,26 @@
 angular.module("todoListApp", [])
+
   .controller('mainCtrl', function($scope) {
     $scope.helloWorld = function() {
       console.log("Hello World! This is the hello-world controller function in the mainCtrl!");
     };
+  })
+
+  .controller('coolCtrl', function($scope) {
+    $scope.whoAmI = function() {
+      console.log("hello there, this is the coolCtrl function!");
+    };
+
+    $scope.helloWorld = function() {
+      console.log("This is not the main ctrl!");
+    };
+
+  })
+
+  .controller('imASibling', function($scope) {
+
+    $scope.foobar = 1234;
+
+    //do other stuff
+
   });
