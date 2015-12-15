9a9fe7f968251a9df867f8cf23095e9f4e237c4e
craigcwaveformlabs <craigmclarke@gmail.com>
2015-12-15 11:16:25 +0000

    adding basic controller with scope

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,6 @@
-angular.module("todoListApp", []);
+angular.module("todoListApp", [])
+  .controller('mainCtrl', function($scope) {
+    $scope.helloWorld = function() {
+      console.log("Hello World! This is the hello-world controller function in the mainCtrl!");
+    };
+  });
